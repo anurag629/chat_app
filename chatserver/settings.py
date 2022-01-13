0570bc1584ea416122a7d0c0ae0ed8837068d6f9
Your Name <you@example.com>
2022-01-13 07:39:27 +0530

    created chatserver project

--- chatserver/settings.py
+++ chatserver/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^omhmm9usn)&7&ovl6244#7ywq@(s_u*#6xa8^$@g^9vkh&3q8'
+SECRET_KEY = 'django-insecure-(^kl#)t2jv7z62m)qbktl6w(_6s#$1zkou9oilqxz_nq&b@$o*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
